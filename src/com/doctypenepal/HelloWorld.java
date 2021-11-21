eeca1c6ce8387c75137aac00bb0771d8eedd70c4
pratiksh404 <pratikdai404@gmail.com>
2021-11-21 18:02:35 +0545

    user input

--- src/com/doctypenepal/HelloWorld.java
+++ src/com/doctypenepal/HelloWorld.java
@@ -2,6 +2,6 @@ package com.doctypenepal;
 
 public class HelloWorld {
     public HelloWorld(){
-        System.out.print("Hello World");
+        System.out.println("Hello World");
     }
 }
--- src/com/doctypenepal/Main.java
+++ src/com/doctypenepal/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // Uncomment to run
 	  HelloWorld helloWorld = new HelloWorld(); // Print hello world in constructor
+        UserInput userInput = new UserInput(); // Take input and display
     }
 }

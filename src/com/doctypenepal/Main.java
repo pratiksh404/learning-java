e531d2a4f1f1ad0cd4ab71750f8e1383097a856e
pratiksh404 <pratikdai404@gmail.com>
2021-11-21 15:28:29 +0545

    Learning Java

---
+++ src/com/doctypenepal/Main.java
@@ -0,0 +1,9 @@
+package com.doctypenepal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Uncomment to run
+	  HelloWorld helloWorld = new HelloWorld(); // Print hello world in constructor
+    }
+}

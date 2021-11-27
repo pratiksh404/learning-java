a19a1a295d54bcded046d9f062e2fb3882a774f6
pratiksh404 <pratikdai404@gmail.com>
2021-11-27 09:21:45 +0545

    string methods

--- src/com/doctypenepal/Main.java
+++ src/com/doctypenepal/Main.java
@@ -9,6 +9,7 @@ public class Main {
         // DialogBox dialogBox = new DialogBox(); // Simple Dialog Box
         // BasicMath basicMath = new BasicMath(); // Basic Mathematical Operation
         // RandomNumber randomNumber = new RandomNumber(); // Random Number Generation
-        MultidimentionalArray multidimentionalArray = new MultidimentionalArray(); // Multi-dimentional Array example
+        // MultidimentionalArray multidimentionalArray = new MultidimentionalArray(); // Multi-dimentional Array example
+        StringMethod stringMethod = new StringMethod();
     }
 }
